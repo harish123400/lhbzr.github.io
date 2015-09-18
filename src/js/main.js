2af41529e7fe08835dec3b550d087a8aea846269
Luis Henrique Bizarro <Luis@dotcore.com.br>
2015-09-18 15:31:19 -0300

    :laughing:

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,60 @@
 (function() {
+  //Functions.
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function replaceAt(value, index, char) {
+    return value.substr(0, index) + char + value.substr(index + 1);
+  }
+
+
+  //Link.
+  var headerLink = document.querySelectorAll('.header-link');
+  var linkOverInterval, linkOutInterval;
+
+  for (var i = 0; i < headerLink.length; i++) {
+    headerLink[i].addEventListener('mouseover', function() {
+      var link = this;
+
+      linkOverInterval = setInterval(function() {
+        var linkValue = link.innerHTML.trim();
+
+        link.innerHTML = replaceAt(linkValue, randomInt(0, linkValue.length - 1), String.fromCharCode(randomInt(65, 122)));
+      }, 50)
+    });
+
+    headerLink[i].addEventListener('mouseout', function() {
+      clearInterval(linkOverInterval);
+
+      var link = this,
+          linkText = link.getAttribute('data-text');
+
+      var i = 0;
+
+      var linkOutInterval = setInterval(function() {
+        if (i < linkText.length) {
+          var linkValue = link.innerHTML.trim();
+
+          link.innerHTML = replaceAt(linkValue, i, linkText[i]);
+        } else {
+          clearInterval(linkOutInterval);
+        }
+
+        i++;
+      }, 50);
+    });
+  }
+
+  //Canvas.
   var windowWidth = window.innerWidth;
   var windowHeight = window.innerHeight;
 
   var header = document.querySelector('.header');
-  var headerWidth = header.offsetWidth;
-  var headerHeight = header.offsetHeight;
   var headerCanvas = document.querySelector('.header-canvas');
 
   var scene, camera, renderer, light, composer, effect;
   var circle, triangle, triangleSleeve, particles, particle;
-
-
-  function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
 
   function init() {
     scene = new THREE.Scene();
@@ -25,7 +65,7 @@
     scene.add(camera);
 
     renderer = new THREE.WebGLRenderer({ canvas: headerCanvas });
-    renderer.setSize(headerWidth, headerHeight);
+    renderer.setSize(windowWidth, windowHeight);
     renderer.setClearColor(0x202020);
 
     light = new THREE.DirectionalLight(0xffffff, 1);
@@ -55,9 +95,9 @@
       circle.add(triangleSleeve[i]);
     }
 
-    for (var i = 0; i < 100; i++) {
-      scene.add(particle);
-    }
+    //    for (var i = 0; i < 100; i++) {
+    //      scene.add(particle);
+    //    }
 
     scene.add(circle);
 
@@ -72,6 +112,10 @@
     effect.uniforms['amount'].value = 0.005;
     effect.renderToScreen = true;
     composer.addPass(effect);
+
+    //    effect = new THREE.GlitchPass();
+    //    effect.renderToScreen = true;
+    //    composer.addPass(effect);
 
     renderer.render(scene, camera);
   }
@@ -104,6 +148,6 @@
     camera.aspect = windowWidth / windowHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(headerWidth, headerHeight);
+    renderer.setSize(windowWidth, windowHeight);
   });
 })();
--- src/js/util.js
+++ src/js/util.js
@@ -172,6 +172,110 @@ THREE.RGBShiftShader = {
 };
 
 /**
+ * @author felixturner / http://airtight.cc/
+ *
+ * RGB Shift Shader
+ * Shifts red and blue channels from center in opposite directions
+ * Ported from http://kriss.cx/tom/2009/05/rgb-shift/
+ * by Tom Butterworth / http://kriss.cx/tom/
+ *
+ * amount: shift distance (1 is width of input)
+ * angle: shift angle in radians
+ */
+
+THREE.DigitalGlitch = {
+
+  uniforms: {
+
+    "tDiffuse":    { type: "t", value: null },//diffuse texture
+    "tDisp":    { type: "t", value: null },//displacement texture for digital glitch squares
+    "byp":      { type: "i", value: 0 },//apply the glitch ?
+    "amount":    { type: "f", value: 0.08 },
+    "angle":    { type: "f", value: 0.02 },
+    "seed":      { type: "f", value: 0.02 },
+    "seed_x":    { type: "f", value: 0.02 },//-1,1
+    "seed_y":    { type: "f", value: 0.02 },//-1,1
+    "distortion_x":  { type: "f", value: 0.5 },
+    "distortion_y":  { type: "f", value: 0.6 },
+    "col_s":    { type: "f", value: 0.05 }
+  },
+
+  vertexShader: [
+
+    "varying vec2 vUv;",
+    "void main() {",
+    "vUv = uv;",
+    "gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+    "}"
+  ].join( "\n" ),
+
+  fragmentShader: [
+    "uniform int byp;",//should we apply the glitch ?
+
+    "uniform sampler2D tDiffuse;",
+    "uniform sampler2D tDisp;",
+
+    "uniform float amount;",
+    "uniform float angle;",
+    "uniform float seed;",
+    "uniform float seed_x;",
+    "uniform float seed_y;",
+    "uniform float distortion_x;",
+    "uniform float distortion_y;",
+    "uniform float col_s;",
+
+    "varying vec2 vUv;",
+
+
+    "float rand(vec2 co){",
+    "return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);",
+    "}",
+
+    "void main() {",
+    "if(byp<1) {",
+    "vec2 p = vUv;",
+    "float xs = floor(gl_FragCoord.x / 0.5);",
+    "float ys = floor(gl_FragCoord.y / 0.5);",
+    //based on staffantans glitch shader for unity https://github.com/staffantan/unityglitch
+    "vec4 normal = texture2D (tDisp, p*seed*seed);",
+    "if(p.y<distortion_x+col_s && p.y>distortion_x-col_s*seed) {",
+    "if(seed_x>0.){",
+    "p.y = 1. - (p.y + distortion_y);",
+    "}",
+    "else {",
+    "p.y = distortion_y;",
+    "}",
+    "}",
+    "if(p.x<distortion_y+col_s && p.x>distortion_y-col_s*seed) {",
+    "if(seed_y>0.){",
+    "p.x=distortion_x;",
+    "}",
+    "else {",
+    "p.x = 1. - (p.x + distortion_x);",
+    "}",
+    "}",
+    "p.x+=normal.x*seed_x*(seed/5.);",
+    "p.y+=normal.y*seed_y*(seed/5.);",
+    //base from RGB shift shader
+    "vec2 offset = amount * vec2( cos(angle), sin(angle));",
+    "vec4 cr = texture2D(tDiffuse, p + offset);",
+    "vec4 cga = texture2D(tDiffuse, p);",
+    "vec4 cb = texture2D(tDiffuse, p - offset);",
+    "gl_FragColor = vec4(cr.r, cga.g, cb.b, cga.a);",
+    //add noise
+    "vec4 snow = 200.*amount*vec4(rand(vec2(xs * seed,ys * seed*50.))*0.2);",
+    "gl_FragColor = gl_FragColor+ snow;",
+    "}",
+    "else {",
+    "gl_FragColor=texture2D (tDiffuse, vUv);",
+    "}",
+    "}"
+
+  ].join( "\n" )
+
+};
+
+/**
  * @author alteredq / http://alteredqualia.com/
  */
 
@@ -307,6 +411,124 @@ THREE.EffectComposer.prototype = {
 
 	}
 
+};
+
+/**
+
+ */
+
+THREE.GlitchPass = function ( dt_size ) {
+
+  if ( THREE.DigitalGlitch === undefined ) console.error( "THREE.GlitchPass relies on THREE.DigitalGlitch" );
+
+  var shader = THREE.DigitalGlitch;
+  this.uniforms = THREE.UniformsUtils.clone( shader.uniforms );
+
+  if ( dt_size == undefined ) dt_size = 64;
+
+
+  this.uniforms[ "tDisp" ].value = this.generateHeightmap( dt_size );
+
+
+  this.material = new THREE.ShaderMaterial( {
+    uniforms: this.uniforms,
+    vertexShader: shader.vertexShader,
+    fragmentShader: shader.fragmentShader
+  } );
+
+  console.log( this.material );
+
+  this.enabled = true;
+  this.renderToScreen = false;
+  this.needsSwap = true;
+
+
+  this.camera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+  this.scene  = new THREE.Scene();
+
+  this.quad = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2, 2 ), null );
+  this.scene.add( this.quad );
+
+  this.goWild = false;
+  this.curF = 0;
+  this.generateTrigger();
+
+};
+
+THREE.GlitchPass.prototype = {
+
+  render: function ( renderer, writeBuffer, readBuffer, delta ) {
+
+    this.uniforms[ "tDiffuse" ].value = readBuffer;
+    this.uniforms[ 'seed' ].value = Math.random();//default seeding
+    this.uniforms[ 'byp' ].value = 0;
+
+    if ( this.curF % this.randX == 0 || this.goWild == true ) {
+
+      this.uniforms[ 'amount' ].value = Math.random() / 30;
+      this.uniforms[ 'angle' ].value = THREE.Math.randFloat( - Math.PI, Math.PI );
+      this.uniforms[ 'seed_x' ].value = THREE.Math.randFloat( - 1, 1 );
+      this.uniforms[ 'seed_y' ].value = THREE.Math.randFloat( - 1, 1 );
+      this.uniforms[ 'distortion_x' ].value = THREE.Math.randFloat( 0, 1 );
+      this.uniforms[ 'distortion_y' ].value = THREE.Math.randFloat( 0, 1 );
+      this.curF = 0;
+      this.generateTrigger();
+
+    } else if ( this.curF % this.randX < this.randX / 5 ) {
+
+      this.uniforms[ 'amount' ].value = Math.random() / 90;
+      this.uniforms[ 'angle' ].value = THREE.Math.randFloat( - Math.PI, Math.PI );
+      this.uniforms[ 'distortion_x' ].value = THREE.Math.randFloat( 0, 1 );
+      this.uniforms[ 'distortion_y' ].value = THREE.Math.randFloat( 0, 1 );
+      this.uniforms[ 'seed_x' ].value = THREE.Math.randFloat( - 0.3, 0.3 );
+      this.uniforms[ 'seed_y' ].value = THREE.Math.randFloat( - 0.3, 0.3 );
+
+    } else if ( this.goWild == false ) {
+
+      this.uniforms[ 'byp' ].value = 1;
+
+    }
+    this.curF ++;
+
+    this.quad.material = this.material;
+    if ( this.renderToScreen ) {
+
+      renderer.render( this.scene, this.camera );
+
+    } else {
+
+      renderer.render( this.scene, this.camera, writeBuffer, false );
+
+    }
+
+  },
+  generateTrigger: function() {
+
+    this.randX = THREE.Math.randInt( 120, 240 );
+
+  },
+  generateHeightmap: function( dt_size ) {
+
+    var data_arr = new Float32Array( dt_size * dt_size * 3 );
+    console.log( dt_size );
+    var length = dt_size * dt_size;
+
+    for ( var i = 0; i < length; i ++ ) {
+
+      var val = THREE.Math.randFloat( 0, 1 );
+      data_arr[ i * 3 + 0 ] = val;
+      data_arr[ i * 3 + 1 ] = val;
+      data_arr[ i * 3 + 2 ] = val;
+
+    }
+
+    var texture = new THREE.DataTexture( data_arr, dt_size, dt_size, THREE.RGBFormat, THREE.FloatType );
+    console.log( texture );
+    console.log( dt_size );
+    texture.needsUpdate = true;
+    return texture;
+
+  }
 };
 
 /**
