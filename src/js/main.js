96680ecdb4075b6944c6eaf7dd757e37ecfd52cc
lhbzr <luis@dotcore.com.br>
2015-10-28 22:57:43 -0200

    :laughing:

--- src/js/main.js
+++ src/js/main.js
@@ -22,7 +22,7 @@ var music = new Music(),
     musicNext = document.querySelector('.music-next');
 
 music.audio.addEventListener('ended', function() {
-  music.load();
+  music.load(music.songNext);
 });
 
 musicToggle.addEventListener('click', function(e) {
